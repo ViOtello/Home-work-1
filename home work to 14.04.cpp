f709c956fbc347df02981c5f128b5a2f0fa16686
Vitaly Gulyaev <vi_gulyaev@mail.ru>
2022-04-10 21:55:50 +0300

    Сделал работу

---
+++ home work to 14.04.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using std::cout;
+
+int main()
+{
+	const int size = 10;
+	int massive[size] = {};
+
+	srand(time(0));
+
+	for (int i = 0; i < size; i++) {
+		massive[i] = rand() % 20;
+		cout << massive[i] << "  ";
+	}
+	cout << "\n";
+
+}
